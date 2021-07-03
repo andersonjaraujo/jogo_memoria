2238c0a955a3f4f7e4189189bf302eeae03e2514
Anderson Araujo <anderson.araujo@arapiraca.ufal.br>
2021-07-03 16:28:26 -0300

    Atualização

--- inst/shinyapp/server.r
+++ inst/shinyapp/server.r
@@ -1,39 +1,7 @@
 library("shiny")
 source("global.R")
 
-ui <- fluidPage(
-  
-  tags$head(
-    tags$link(href="styles.css", rel="stylesheet", type="text/css"),
-    tags$script(src = "http://platform.twitter.com/widgets.js"),
-    includeHTML(path = "www/google-analytics.html")
-  ),
-  
-  tags$div(
-    class = "title-app",
-    tags$h1("Jogo da Memória"),
-    tags$h4("Encontre a foto correspontente!")
-  ),
-  tags$br(),
-  
-  # verbatimTextOutput("test_res_show"),
-  
-  tags$div(
-    style = "width: 650px; margin: auto;",
-    time_UI("timer"),
-    tags$br(),
-    lapply(
-      X = seq_len(n_hex * 2),
-      FUN = function(x) {
-        hex_UI(id = paste0("module", x))
-      }
-    )#,
-    # verbatimTextOutput("test_res")
-  )
-  
-)
-
-server <- function(input, output, session) {
+function(input, output, session) {
   
   start <- callModule(module = welcome, id = "Bem feito !")
   timer <- callModule(module = time, id = "timer", start = start)
@@ -142,5 +110,3 @@ server <- function(input, output, session) {
     session$reload()
   }, ignoreInit = TRUE)
 }
-
-shinyApp(ui = ui, server = server)
